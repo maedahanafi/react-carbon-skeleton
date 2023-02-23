c1599a1aa9968eb902999e859e96288c2a0674f8
Maeda Hanafi <Maeda.Hanafi@ibm.com>
2023-02-23 14:39:04 -0500

    more updates

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,45 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import myObjectReducer from "./myObjectReducer";
+import anotherObjectReducer from "./anotherObjectReducer";
 
-const myObjectInitialState = {
-  myObject: null,
-};
 
-function myObjectReducer(state = myObjectInitialState, action) {
-  switch (action.type) {
-    case "SET_MY_OBJECT":
-      return {
-        ...state,
-        myObject: action.payload.myObject,
-      };
-    case "DELETE_MY_OBJECT":
-      return {
-        ...state,
-        myObject: null,
-      };
-    default:
-      return state;
-  }
-}
-
-const anotherObjectInitialState = {
-  anotherObjectArr: []
-};
-
-function anotherObjectReducer(state = anotherObjectInitialState, action) {
-  switch (action.type) {
-    case "ADD_ANOTHER_OBJECT":
-
-      return {
-        ...state, 
-        anotherObjectArr: [
-          ...state.anotherObjectArr, 
-          action.payload.anotherObject
-        ]
-      };
-    default:
-      return state;
-  }
-}
 
 
 export const store =  configureStore({
